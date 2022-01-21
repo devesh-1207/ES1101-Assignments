65ac515024880a2f57aee3a5b605653b97e9cda9
devesh-1207 <83128892+devesh-1207@users.noreply.github.com>
2022-01-21 17:42:37 +0530

    Create Question_4.py

---
+++ Question_4.py
@@ -0,0 +1,13 @@
+#Question 4
+
+marks1 = int(input('marks of student1: '))
+marks2 = int(input('marks of student2: '))
+marks3 = int(input('marks of student3: '))
+marks4 = int(input('marks of student4: '))
+marks5 = int(input('marks of student5: '))
+#before sorting
+list = [marks1,marks2,marks3,marks4,marks5] 
+print(list)
+list.sort()
+#after sorting
+print('list after sorting', list) 
