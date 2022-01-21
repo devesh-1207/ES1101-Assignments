68d052a048922040fd24f43b504c9bf03d1bf86c
devesh-1207 <83128892+devesh-1207@users.noreply.github.com>
2022-01-21 17:37:31 +0530

    Create Question_1.py

---
+++ Question_1.py
@@ -0,0 +1,8 @@
+# PYTHON ASSIGNMENT-1
+#QUESTION-1
+
+number1 = int(input('number1: '))
+number2 = int(input('number2: '))
+number3 = int(input('number3: '))
+average = (number1 +number2 + number3)/3
+print(average)
