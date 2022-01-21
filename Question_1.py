3a1da3fd035e66cdb4c12d57c7023cf4f96904e7
devesh-1207 <83128892+devesh-1207@users.noreply.github.com>
2022-01-21 17:38:22 +0530

    Update Question_1.py

--- Question_1.py
+++ Question_1.py
@@ -4,5 +4,5 @@
 number1 = int(input('number1: '))
 number2 = int(input('number2: '))
 number3 = int(input('number3: '))
-average = (number1 +number2 + number3)/3
+average = (number1 + number2 + number3)/3
 print(average)
