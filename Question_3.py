ff9bbc15a0844a225aa21d9e3b9a35568b1250b4
devesh-1207 <83128892+devesh-1207@users.noreply.github.com>
2022-01-21 17:41:11 +0530

    Create Question_3.py

---
+++ Question_3.py
@@ -0,0 +1,9 @@
+# Question 3
+name = input('what is your name?:  ')
+gender = input('gender: ')
+SID = input('SID: ')
+course_name = input('your branch: ')
+cgpa = input('your CGPA: ')
+ #creating a list
+student = [name,gender,SID,course_name,cgpa]
+print(student)
