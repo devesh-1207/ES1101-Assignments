370e92c9f937735590510f27ff3b8cf85754b18a
devesh-1207 <83128892+devesh-1207@users.noreply.github.com>
2022-01-21 17:45:17 +0530

    Create Question_5.py

---
+++ Question_5.py
@@ -0,0 +1,10 @@
+#Question 5
+#part 1
+colour = [ 'red','green','white','black','pink','yellow']
+colour.pop(3)
+print(colour)
+
+#part 2
+
+colour[2] = 'purple'; colour[3] = 'purple'
+print(colour)
